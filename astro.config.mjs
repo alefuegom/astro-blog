d5f9338957d723916d47d7e139cc186b95311fee
Alejandro Fuentes Gómez <alejandrofuegom@gmail.com>
2024-10-29 21:29:59 +0100

    Feature - Header - Install icon dependences and usage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 export default defineConfig({
+  integrations: [react()],
   i18n: {
     defaultLocale: "en",
     locales: ["es", "en"],
